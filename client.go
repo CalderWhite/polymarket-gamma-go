329f2a8c631d4e7641a115aa72b5775b7c7ae5cc
Calder White <calderwhite1@gmail.com>
2025-11-10 21:29:28 -0500

    Add package-level documentation for pkg.go.dev

--- client.go
+++ client.go
@@ -1,3 +1,39 @@
+// Package polymarket_gamma provides a Go client for interacting with the Polymarket Gamma structure API.
+//
+// This package:
+//   - Does not error when new fields are added
+//   - Will validate known fields
+//   - May become out of date (please make a PR!)
+//   - Supports querying by event, which is Polymarket's recommended method for market & event discovery
+//
+// # Installation
+//
+//	go get github.com/CalderWhite/polymarket-gamma-go
+//
+// # Usage
+//
+// Fetching events by ID:
+//
+//	client := polymarket_gamma.NewClient(nil)
+//	response, err := client.GetEventsByIDs([]int{2890, 2891, 2892})
+//	if err != nil {
+//	    log.Fatal(err)
+//	}
+//
+//	for _, event := range response.Events {
+//	    fmt.Printf("Event: %s\n", event.Title)
+//	    for _, market := range event.Markets {
+//	        fmt.Printf("  Market: %s\n", market.Question)
+//	    }
+//	}
+//
+// Pagination (for event/market discovery):
+//
+//	// Fetch the latest 10 events (by ID)
+//	response, err := client.GetEventsByPage(0, 10, false)
+//
+//	// Fetch events in order
+//	response, err := client.GetEventsByPage(0, 10, true)
 package polymarket_gamma
 
 import (
